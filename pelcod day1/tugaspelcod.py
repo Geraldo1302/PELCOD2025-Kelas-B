db4dc265aecf9d9fa5a9aebfcec5ab024a55908b
Geraldo1302 <tobingaldo198@gmail.com>
2025-09-17 15:13:12 +0700

    250441100040_Geraldo_pelcod

---
+++ pelcod day1/tugaspelcod.py
@@ -0,0 +1,22 @@
+a=85
+b=75        
+c=90
+d=a*30/100
+e=b*30/100
+f=c*40/100
+g=d+e+f
+
+print("----Nilai Yang Kamu Peroleh-----")
+print("Nilai tugas :"  ,a)
+print("Nilai UTS :" ,b)
+print("Nilai UAS :" ,c)
+
+print("--------------------------------")
+
+print("Nilai akhir tugas (30%) :" ,d)
+print("Nilai akhir UTS (30%) :" ,e)
+print("Nilai akhir UAS (40%) :" ,f)
+
+print("--------------------------------")
+
+print("Total nilai kamu :",g)
