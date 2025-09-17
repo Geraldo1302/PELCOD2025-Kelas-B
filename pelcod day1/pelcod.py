4f2a0a93d0bc6d0cb3cdf9f4f8a61495103c6287
Geraldo1302 <tobingaldo198@gmail.com>
2025-09-17 15:51:41 +0700

    250441100040_Geraldo_pelcod

---
+++ pelcod day1/pelcod.py
@@ -0,0 +1,30 @@
+nama= "geraldo pandapotan lumbantobing"
+nim="250441100040"
+semester=12
+
+print("=====PRINT BIODATA MAHASISWA=====")
+print("hallo nama saya : "+ nama + "\n")
+print(f"NIM saya : {nim}\n")
+print("saya berkuliah di semester : {}".format(semester) + " di universitas diponegoro ")
+
+a=10
+b=5
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+
+print("hasil penjumlahan:",c)
+print("hail pengurangan:",d)
+print("hasil perkalian:",e)
+print("hasil pembagian:",f)
+print("hasil modulus:",g)
+
+nama = input("masukkan nama anda:")
+umur = input("masukkan umur anda: ")
+buah = input("masukkan buah favorit anda: ")
+
+print("nama saya adalah " + nama) 
+print("umur saya adalah " + umur + " tahun ")
+print("buah favorit saya adalah " + buah)
